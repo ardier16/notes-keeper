e681a9803608fcf1186101adecdd600990615e4a
Maxim Shopinsky <ardier16@gmail.com>
2016-04-05 09:18:05 +0300

    Update modules.php

--- modules.php
+++ modules.php
@@ -2,7 +2,7 @@
 	function signup($mail, $password)
 	{
 		$con = new MongoClient();
-		$db = $con-> test4->users;
+		$db = $con-> db->users;
 
 		$data = array(
 						"mail" => $mail,
@@ -24,7 +24,7 @@
 	function login($mail, $password)
 	{
 		$con = new MongoClient($mail, $password);
-		$db = $con-> test4->users;
+		$db = $con-> db->users;
 		$log = false;
 		$data = array("mail" => $mail, 
 					  "password" => $password);
@@ -47,7 +47,7 @@
 	function add_note($id, $title, $text)
 	{
 		$con = new MongoClient();
-		$notes = $con-> test4->notes;
+		$notes = $con-> db->notes;
 		$note = array("id" => $id,
 					  "title" => $title,
 					  "text" => $text);
@@ -55,22 +55,22 @@
 		$notes->insert($note);
 	}
 
-	function remove_note($id, $title, $text)
+	function remove_note($_id)
 	{
 		$con = new MongoClient();
-		$notes = $con-> test4->notes;
-		$info = array("id" => $id, "title" => $title, "text" => $text);
+		$notes = $con-> db->notes;
+		$info = array("_id" => new MongoId($_id));
 		$notes->remove($info, array('justOne' => true));
 	}
 
-	function update_note($id, $oldtitle, $oldtext, $title, $text)
+	function update_note($_id, $title, $text)
 	{
 		$con = new MongoClient();
-		$notes = $con-> test4->notes;
-		$old = array("id" => $id, "title" => $oldtitle, "text" => $oldtext);
-		$new = array("id" => $id, "title" => $title, "text" => $text);
-		$notes->update($old, $new, array("upsert" => true));
-	}		
+		$notes = $con-> db->notes;
+		$old = array("_id" => new MongoId($_id));
+		$new = array("title" => $title, "text" => $text);
+		$notes->update($old, array('$set' => $new), array("upsert" => true));
+	}
 ?>
 
 
